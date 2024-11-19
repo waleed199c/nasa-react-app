a934276f7b9ff1dfcdade4f42cba1c913d30725b
Alwaleed Alshgnoubi <alwaleedshg@gmail.com>
2024-11-20 01:50:58 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   useEffect(() => {
     async function fetchAPIData() {
-      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      const NASA_KEY = process.env.VITE_NASA_API_KEY
       const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
 
 
